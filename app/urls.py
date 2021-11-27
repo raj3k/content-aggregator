95c7c17eb930ade393a99b91de88d965782c16bb
raj3k <rajkosky@gmail.com>
2021-11-27 07:58:05 +0100

    added update_programming_medium function in views and modified index page

--- app/urls.py
+++ app/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import index, update_python_medium
+from .views import index, update_python_medium, update_programming_medium
 
 
 urlpatterns = [
     path('', index, name='index_page'),
-    path('python_medium/', update_python_medium, name='update-python-medium')
+    path('python_medium/', update_python_medium, name='update-python-medium'),
+    path('programming_medium/', update_programming_medium, name='update-programming-medium'),
 ]
--- app/views.py
+++ app/views.py
@@ -1,13 +1,15 @@
 from django.shortcuts import render, redirect
-from .models import PythonMediumContent
+from .models import PythonMediumContent, ProgrammingMediumContent
 import feedparser
 
 
 # Create your views here.
 def index(request):
     python_medium_content = PythonMediumContent.objects.all()
+    programming_medium_content = ProgrammingMediumContent.objects.all()
     context = {
-        'py_content': python_medium_content
+        'py_content': python_medium_content,
+        'programming_content': programming_medium_content,
     }
     return render(request, 'index.html', context)
 
@@ -17,6 +19,24 @@ def update_python_medium(request):
 
     for i in range(10):
         model = PythonMediumContent()
+        data = feed.entries[i]
+
+        model.title = data.title
+        model.url = data.link
+        # Image
+        start_img_index = (data.summary).find('img')+9
+        end_img_index = (data.summary).find('width')-2
+        model.img = data.summary[start_img_index:end_img_index]
+
+        model.save()
+    return redirect('/')
+
+
+def update_programming_medium(request):
+    feed = feedparser.parse('https://medium.com/feed/tag/programming')
+
+    for i in range(10):
+        model = ProgrammingMediumContent()
         data = feed.entries[i]
 
         model.title = data.title
