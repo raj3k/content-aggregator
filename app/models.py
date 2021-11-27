b5c93b5479d557b46755bbc3919cc3155bbca8de
raj3k <rajkosky@gmail.com>
2021-11-27 07:44:54 +0100

    added ProgrammingMediumContent model

--- app/models.py
+++ app/models.py
@@ -9,3 +9,12 @@ class PythonMediumContent(models.Model):
 
     def __str__(self):
         return self.title
+
+
+class ProgrammingMediumContent(models.Model):
+    title = models.CharField(max_length=300)
+    url = models.TextField()
+    img = models.URLField(null=True, blank=True)
+
+    def __str__(self):
+        return self.title
